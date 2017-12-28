947c836b60bf94c043038c23911b54e8cf741f7f
Alejandro Pérez Martín <aleperez92@gmail.com>
2017-12-28 23:29:42 +0100

    Added variables test

---
+++ test/variables.test.js
@@ -0,0 +1,51 @@
+const tap = require('tap');
+const ClientKitCss = require('../');
+const utils = require('./utils');
+
+tap.test('variables', (t) => {
+  const css = new ClientKitCss('vars', {
+    vars: {
+      'line-height': 1
+    }
+  });
+  t.deepEqual(css.cssVars, {
+    'line-height': 1
+  });
+  t.end();
+});
+
+tap.test('nested variables', (t) => {
+  const css = new ClientKitCss('vars', {
+    vars: {
+      font: {
+        primary: 'Arial',
+        secondary: 'Helvetica'
+      }
+    }
+  });
+  t.deepEqual(css.cssVars, {
+    'font-primary': 'Arial',
+    'font-secondary': 'Helvetica'
+  });
+  t.end();
+});
+
+tap.test('variables in css', (t) => {
+  const css = new ClientKitCss('css', {
+    files: {
+      'test/out/variables.css': 'test/fixtures/variables.css'
+    },
+    vars: {
+      'line-height': 1,
+      font: {
+        primary: 'Arial',
+        secondary: 'Helvetica'
+      }
+    }
+  });
+  css.execute((err, results) => {
+    t.equal(err, null);
+    utils.checkOutput(t, 'variables.css');
+    t.end();
+  });
+});
